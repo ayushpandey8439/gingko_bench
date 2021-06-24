f130c13043743c4919d55d3cb16ebee3e3ff3637
Ayush Pandey <ayushpandey8439@gmail.com>
2021-06-24 23:10:24 +0200

    Remove build files

--- _build/default/plugins/rebar3_format/src/rebar3_format.erl
+++
@@ -1,9 +0,0 @@
-%%% @doc Main entry point for the rebar3 format plugin
--module(rebar3_format).
-
--export([init/1]).
-
-%% @private
--spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
-init(State) ->
-    rebar3_format_prv:init(State).
